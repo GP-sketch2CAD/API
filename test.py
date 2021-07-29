40bc55c921860bd58537a735d5a225d5eeb32eb2
Young2218 <young221718@gmail.com>
2021-07-29 19:51:08 +0900

    좌표 찍으면 캐드 파일 생성됨

--- test.py
+++ test.py
@@ -1,36 +1,5 @@
-import ezdxf
-
-# Create a new DXF document.
-doc = ezdxf.new(dxfversion='R2010')
-
-# Create new table entries (layers, linetypes, text styles, ...).
-doc.layers.new('TEXTLAYER', dxfattribs={'color': 2})
-
-# DXF entities (LINE, TEXT, ...) reside in a layout (modelspace, 
-# paperspace layout or block definition).  
-msp = doc.modelspace()
-
-# Add entities to a layout by factory methods: layout.add_...() 
-msp.add_line((0, 0), (2000, 0), dxfattribs={'color': 7})
-msp.add_line((2000, 0), (0, 100), dxfattribs={'color': 7})
-msp.add_line((100, 0), (100, 100), dxfattribs={'color': 7})
-msp.add_line((0, 100), (100, 100), dxfattribs={'color': 7})
 
 
-msp2 = doc.modelspace()
+lst = [1,2,3,4,5]
 
-# Add entities to a layout by factory methods: layout.add_...() 
-msp2.add_line((230, 0), (100,230), dxfattribs={'color':17})
-msp2.add_line((0,230), (0, 12300), dxfattribs={'color': 17})
-msp2.add_line((10230, 0), (100, 12300), dxfattribs={'color': 17})
-msp2.add_line((0, 10230), (100, 1000), dxfattribs={'color': 17})
-
-# msp.add_text(
-#     'Test', 
-#     dxfattribs={
-#         'layer': 'TEXTLAYER'
-#     }).set_pos((0, 0.2), align='CENTER')
-
-# Save DXF document.
-doc.saveas('test.dxf')
-
+print(lst[3])
